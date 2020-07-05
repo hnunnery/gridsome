742b6f2c874895de0afe6cbe45e8b4065e27eddd
Hunter Nunnery <hunter.nunnery@yahoo.com>
2020-07-05 10:51:30 -0400

    Update Index.vue

--- src/pages/Index.vue
+++ src/pages/Index.vue
@@ -7,7 +7,7 @@
         v-for="section in sections"
         :key="section.node.id"
       >
-        <v-col cols="12" lg="10" xl="8" class="mb-12">
+        <v-col class="mb-12">
           <h1
             class="custom-header mont secondary--text text-center mt-md-5 mb-5 mb-md-12"
             style="line-height: 1em;"
@@ -20,6 +20,34 @@
           <p class="body-1 secondary--text mt-6" style="line-height: 2em;">
             {{ section.node.paraTwo }}
           </p>
+        </v-col>
+      </v-row>
+
+      <v-row class="justify-center my-12 pb-12">
+        <v-col
+          cols="12"
+          sm="6"
+          md="4"
+          lg="3"
+          v-for="post in posts.slice(0, 4)"
+          :key="post.node.id"
+          @click="onClick(post)"
+          style="cursor: pointer;"
+        >
+          <v-card flat>
+            <v-img
+              :src="post.node.featuredImage"
+              :alt="post.node.alt"
+              class="mx-auto mb-6"
+              lazy-src
+            ></v-img>
+            <h3
+              class="title secondary--text text-center mb-6"
+              style="line-height: 1em;"
+            >
+              {{ post.node.title }}
+            </h3>
+          </v-card>
         </v-col>
       </v-row>
     </v-container>
@@ -39,6 +67,21 @@ query {
       }
     }
   }
+  allPost {
+    totalCount
+    edges {
+      node {
+        id
+        title
+        path
+        date
+        content
+        preview
+        featuredImage
+        alt
+      }
+    }
+  }
 }
 </page-query>
 
@@ -50,6 +93,17 @@ export default {
   computed: {
     sections() {
       return this.$page.allHome.edges;
+    },
+    posts() {
+      const x = this.$page.allPost.edges.sort((a, b) =>
+        Date.parse(a.node.date) > Date.parse(b.node.date) ? -1 : 1
+      );
+      return x;
+    },
+  },
+  methods: {
+    onClick(post) {
+      this.$router.push({ path: post.node.path });
     },
   },
 };
